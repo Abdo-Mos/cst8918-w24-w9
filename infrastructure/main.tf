db554b3a4355ce2fa0ec686ea2ec45a02783b27b
Abdo-Mos <aabbddoo.sshh@gmail.com>
2024-03-08 21:20:15 -0500

    fix GHA err

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_resource_group" "resource_group" {
 resource "azurerm_storage_account" "storageacct" {
   name                     = "lab9storageacc"
   resource_group_name      = azurerm_resource_group.resource_group.name
-  location                     = azurerm_resource_group.resource_group.location
+  location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
