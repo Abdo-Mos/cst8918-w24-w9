c11c750fa1e08b7e76585fcf480db6097f9efcb6
Abdo-Mos <aabbddoo.sshh@gmail.com>
2024-03-08 21:14:02 -0500

    fixed issues

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,9 +23,9 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "storageacct" {
-  name    = "lab9storageacc"
-  resource_group_name               = azurerm_resource_group.resource_group.name
-  location            = azurerm_resource_group.resource_group.location
+  name                     = "lab9storageacc"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
