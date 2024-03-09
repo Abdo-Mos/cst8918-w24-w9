b2134cbbfa8d05b60a8b0b97372ad8a872ccab8d
Abdo-Mos <aabbddoo.sshh@gmail.com>
2024-03-08 21:11:47 -0500

    GHAction 2nd test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -22,10 +22,10 @@ resource "azurerm_resource_group" "resource_group" {
   location = "Canada Central"
 }
 
-resoure "azurerm_storage_account" "storageacct" {
-  name                     = "lab9storageacc"
-  resource_group_name      = azurerm_resource_group.resource_group.name
-  location                 = azurerm_resource_group.resource_group.location
+resource "azurerm_storage_account" "storageacct" {
+  name    = "lab9storageacc"
+  resource_group_name               = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
